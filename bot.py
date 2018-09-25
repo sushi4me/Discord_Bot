ac5cf2161e8873876195dc9b7364658560cee454
Nathan Kong <nathankong1688@yahoo.com>
2018-09-25 09:03:00 -0700

    tab correction

--- bot.py
+++ bot.py
@@ -10,19 +10,21 @@ class ServerBot():
 
     @bot.event
     async def on_ready():
-        dprint('Logged in as: {0}#{1}\n{2}'.format(bot.user.name, bot.user.id, DIVIDER))
+        dprint('Logged in as: {0}, {1}\n{2}'.format(bot.user.name, bot.user.id, DIVIDER))
         # Set the bot's status and activity (different with the rewritten Discord lib)
         await bot.change_presence(status=Status.online, 
             activity=Game(name="Literally Botting"))
 
     @bot.event
     async def on_message(message):
+        # Messages sent by the bot are ignored
         if message.author == bot.user:
             return
+        # Bot will response given these patterns in a message
         if message.content.lower() == "where is bryant?":
             await message.channel.send("Late.")
-
-    await bot.process_commands(message)
+        # If a message does not match the above, attempt to parse as command
+        await bot.process_commands(message)
 
     @bot.command()
     async def echo(ctx):
