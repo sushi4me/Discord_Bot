50493a956a9269d6217668b0e6960095e9a0d68d
Nathan Kong <nathankong1688@yahoo.com>
2018-09-25 13:48:10 -0700

    small edit

--- debug.py
+++ debug.py
@@ -1,7 +1,7 @@
 import os
 
 def dprint(message):
-    if 'debug' in os.environ && os.environ['debug'].lower() == "true":
+    if 'debug' in os.environ and os.environ['debug'].lower() == "true":
         print('[DEBUG] {0}'.format(message))
     else:
         return
