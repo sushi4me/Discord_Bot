3187e593a9099c939298cbfd99ee84eacc7e9160
Nathan Kong <nathankong1688@yahoo.com>
2018-09-25 09:20:38 -0700

    quick changes

--- bot.py
+++ bot.py
@@ -6,31 +6,31 @@ DIVIDER = "----"
 
 class ServerBot():
     def __init__(self, command_prefix, description):
-        self.bot = commands.Bot(command_prefix=command_prefix, description=description)
+        bot = commands.Bot(command_prefix=command_prefix, description=description)
         dprint('Success!')
 
-    @self.bot.event
-    async def on_ready(self):
-        dprint('Logged in as: {0}, {1}\n{2}'.format(self.bot.user.name, self.bot.user.id, DIVIDER))
-        # Set the bot's status and activity (different with the rewritten Discord lib)
-        await bot.change_presence(status=Status.online, 
-            activity=Game(name="Literally Botting"))
+        @bot.event
+        async def on_ready():
+            dprint('Logged in as: {0}, {1}\n{2}'.format(bot.user.name, bot.user.id, DIVIDER))
+            # Set the bot's status and activity (different with the rewritten Discord lib)
+            await bot.change_presence(status=Status.online, 
+                activity=Game(name="Literally Botting"))
 
-    @self.bot.event
-    async def on_message(self, message):
-        # Messages sent by the bot are ignored
-        if message.author == self.bot.user:
-            return
-        # Bot will response given these patterns in a message
-        if message.content.lower() == "where is bryant?":
-            await message.channel.send("Late.")
-        # If a message does not match the above, attempt to parse as command
-        await self.bot.process_commands(message)
+        @bot.event
+        async def on_message(message):
+            # Messages sent by the bot are ignored
+            if message.author == bot.user:
+                return
+            # Bot will response given these patterns in a message
+            if message.content.lower() == "where is bryant?":
+                await message.channel.send("Late.")
+            # If a message does not match the above, attempt to parse as command
+            await bot.process_commands(message)
 
-    @self.bot.command()
-    async def echo(ctx):
-        await ctx.send(":wave: Hello there!")
+        @bot.command()
+        async def echo(ctx):
+            await ctx.send(":wave: Hello there!")
 
-    @self.bot.command()
-    async def cat(ctx):
-        await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+        @bot.command()
+        async def cat(ctx):
+            await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
--- debug.py
+++ debug.py
@@ -1,2 +1,7 @@
+import os
+
 def dprint(message):
-    print('[DEBUG] {0}'.format(message))
+    if 'DEBUG' == True in os.environ:
+        print('[DEBUG] {0}'.format(message))
+    else:
+        return
