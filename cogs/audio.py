7b73517ece79e3c2f8de163c09a0161ce8d9b7a3
Nathan Kong <nathankong1688@yahoo.com>
2018-09-26 08:48:17 -0700

    [Multiple commits] Working on audio cog

--- cogs/audio.py
+++ cogs/audio.py
@@ -26,21 +26,22 @@ class Audio:
 
     @commands.command(name="join")
     async def _join_channel(self, ctx):
-        print("ctx: {0}".format(ctx.__class__.__mro__))
-        print("[1] {0}".format(ctx.message))
-        print("[2] {0}".format(ctx.message.author.voice))
-        print("[3] {0}".format(ctx.message.channel))
-        print("[4] {0}".format(ctx.message.content))
-
-        if ctx.message.author.voice not None:
-            print("Aha! {0} is in {1}".format(ctx.message.author.name, ctx.message.author.voice.channel.name))
-            voice_channel = ctx.message.author.voice.channel
+        if ctx.message.author.voice is not None:
+            print("Aha! {0} is in {1}".format(ctx.message.author.name, 
+                ctx.message.author.voice.channel.name))
+            ctx.message.author.voice.channel.connect(timeout=10, reconnect=True)
         else:
             print("{0} is not in a voice channel".format(ctx.message.author.name))
 
     @commands.command(name="disconnect")
     async def _disconnect(self, ctx):
-        for voice_client in self.server_bot.voice_clients
+        print("ctx: {0}".format(ctx.__class__.__mro__))
+        print("[1] {0}".format(ctx.message))
+        print("[2] {0}".format(ctx.message.author.voice))
+        print("[3] {0}".format(ctx.message.channel))
+        print("[4] {0}".format(ctx.message.content))
+        print("[5] {0}".format(self.server_bot))
+        for voice_client in self.server_bot.voice_clients:
             if voice_client.channel == ctx.message.author.voice.channel:
                 voice_client.disconnect()
 
